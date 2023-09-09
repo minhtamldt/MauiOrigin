48c8de2e752febf97b6601d8ee29e89c3f5bebb0
TamTM <minhtamldt@gmail.com>
2023-09-09 15:28:31 +0700

    init source
    - add gitignore file
    - remove platform not supports

---
+++ MauiOrigin/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiOrigin;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
